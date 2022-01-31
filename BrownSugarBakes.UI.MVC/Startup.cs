8c05079b617c230bc5db132e2bd81083392e3171
State9019 <S.tate19th@gmail.com>
2022-01-31 00:29:07 -0600

    Add project files.

---
+++ BrownSugarBakes.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BrownSugarBakes.UI.MVC.Startup))]
+namespace BrownSugarBakes.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
